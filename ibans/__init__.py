9bdf5c96eeffb6e195165ec2f866da580806805c
Ilton ingui <iltoningui@outlook.com>
2021-03-04 17:28:00 +0100

    Adds country, bank and account models

--- ibans/__init__.py
+++ ibans/__init__.py
@@ -1,3 +1,3 @@
-from .iban import Iban, IbanFormat  # noqa: F401
+from .models import Iban, IbanFormat  # noqa: F401
 from .parser import parse  # noqa: F401
 from .exception import IbanParseException, ExceptionType  # noqa: F401
--- ibans/models.py
+++ ibans/models.py
@@ -8,6 +8,128 @@ class IbanFormat(Enum):
     ELECTRONIC_HIDDEN = 4
 
 
+class Country:
+    def __init__(self, code: str, name: str) -> None:
+        self.__code = code
+        self.__name = name
+        """
+        __init__ Constructs a country object
+
+        :param code: Country alpha 2 code
+        :type code: str
+        :param name: Country Name
+        :type name: str
+        """
+
+    @property
+    def code(self) -> str:
+        return self.__code
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+
+class Account:
+    def __init__(self, number: str, account_type: str = None,
+                 holder: str = None, currency_code: str = None,
+                 balance_account: str = None,
+                 owner_account_number: str = None) -> None:
+        self.__number = number
+        self.__account_type = account_type
+        self.__holder = holder
+        self.__currency_code = currency_code
+        self.__balance_account = balance_account
+        self.__owner_account_number = owner_account_number
+        """
+        __init__ Constructs a bank account
+
+        Provided the Account number and other data
+        :param number: The account number
+        :type number: str
+        :param account_type: The account type
+        :type account_type: str
+        :param holder: Account holder
+        :type holder: str
+        :param currency_code: currency code
+        :type currency_code: str
+        :param balance_account: balance account number
+        :type balance_account: str
+        :param owner_account_number: owner account number
+        :type owner_account_number: str
+        """
+
+    @property
+    def number(self):
+        return self.number
+
+    @property
+    def account_type(self):
+        return self.account_type
+
+    @property
+    def holder(self):
+        return self.holder
+
+    @property
+    def currency_code(self):
+        return self.currency_code
+
+    @property
+    def balance_account(self):
+        return self.balance_account
+
+    @property
+    def owner_account_number(self):
+        return self.owner_account_number
+
+
+class Bank:
+    """
+    Represents a bank in a country
+    """
+    def __init__(self, code: str, name: str, branch: str = None, short_name: str = None, swift: str = None):
+        self.__name = name
+        self.__code = code
+        self.__branch = branch
+        self.__short_name = short_name
+        self.__swift = swift
+        """Constructs a bank object
+        
+        Provided the bank national code and rest of information
+        :param code: Bank national code
+        :type code: str
+        :param name: bank name
+        :type code: str
+        :param branch: Bank branch
+        :type branch: str
+        :param short_name: Bank initials or short name
+        :type short_name: str
+        :param swift: Bank SWIFT/BIC code
+        type swift: str
+        """
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def code(self):
+        return self.__code
+
+    @property
+    def branch(self):
+        return self.__branch
+
+    @property
+    def short_name(self):
+        return self.__short_name
+
+    @property
+    def swift(self):
+        return self.__swift
+
+
 class Iban:
     """
     This class represents a parsed IBAN
@@ -90,7 +212,7 @@ class Iban:
         :type format: IbanFormat
         :return: The IBAN as a formatted string
         :rtype: str
-        """            
+        """
         is_print_hidden = format == IbanFormat.PRINT_HIDDEN
         is_electronic_hidden = format == IbanFormat.ELECTRONIC_HIDDEN
         is_print = format == IbanFormat.PRINT
