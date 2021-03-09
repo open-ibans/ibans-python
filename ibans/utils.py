19936089ad65d7231720ba3df0ed9c2f15702ec2
Ilton ingui <iltoningui@outlook.com>
2021-03-09 23:03:16 +0100

    update utils to use protobuffer specs

--- ibans/utils.py
+++ ibans/utils.py
@@ -1,8 +1,18 @@
-import re
+from typing import Optional
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from specs_pb2 import IbanSpec, BankData
+    from . import Iban, Account, Country, Bank
+    from re import Pattern, Match
+
+from specs_pb2 import BanksData, IbansSpecs
+from . import Iban, Account, Country, Bank
 import pkg_resources
+from .exception import ValueException, ParseException, InvalidBBANException,\
+    InvalidCheckDigitException, InvalidMod97Exception, InvalidReservedValues
+import re
 
-
-letter_to_replace = {
+LETTERS = {
     "A": "10",
     "B": "11",
     "C": "12",
@@ -32,113 +42,139 @@ letter_to_replace = {
 }
 
 
-def get_iban_validation_str(country_code: str) -> list:
-    """
-    get_iban_validation_str retrieves a string from ibans.txt
+def parse(iban_string: str) -> 'Iban':
+    assert iban_string is not None, "Must not be empty"
+    iban_string = iban_string.replace(" ", "")
+    country_code = iban_string[:2]
+    spec: 'IbanSpec' = __get_iban_spec(country_code)
 
-    The string containing the IBAN definition for the selected country
+    if spec is None:
+        raise ValueException("This country doesn't support iban")
 
-    :param country_code: The country alpha-2 code that comes as
-        the first 2 letters of an IBAN
-    :type country_code: str
-    :return: a list containing the IBAN validation data
-    :rtype: list
-    """
+    validate(iban_string, spec)
+
+    iban_regex: 'Pattern' = re.compile(spec.iban_pattern)
+    match: 'Match' = iban_regex.match(iban_string)
+    bank_code: str = match.group(spec.bank_code)
+    check_digit: str = match.group(spec.check_digit)
+    account_number: str = match.group(spec.account_number)
+    branch_code: str = __get_match_group(match, spec.branch_code)
+    account_type: str = __get_match_group(match, spec.branch_code)
+    account_holder_number: str = __get_match_group(match, spec.branch_code)
+    balance_account_number: str = __get_match_group(match, spec.branch_code)
+    currency_code: str = __get_match_group(match, spec.branch_code)
+    owner_account_number: str = __get_match_group(match, spec.branch_code)
+    account: 'Account' = Account(account_number, account_type=account_type,
+                                 holder=account_holder_number, currency_code=currency_code,
+                                 balance_account=balance_account_number,
+                                 owner_account_number=owner_account_number)
+    country: 'Country' = Country(country_code, spec.country_name)
+    bank: 'Bank' = __get_bank(country_code, bank_code, branch_code)
+    basic_bank_account_number: str = iban_string[4:]
+    iban: 'Iban' = Iban(check_digit, basic_bank_account_number, spec.formats, bank,
+                        account, country)
+    return iban
+
+
+def is_valid(iban_string: str, spec: 'IbanSpec') -> bool:
     try:
-        stream = pkg_resources.resource_stream(__name__, 'data/ibans.txt')
-    except FileNotFoundError:
-        return []
-    for line in stream:
-        line = line.decode('utf-8')
-        if country_code == line[0:2]:
-            return line.strip().split("|")
-    return []
+        validate(iban_string, spec)
+    except ParseException:
+        return False
+    return True
 
 
-def bank_validation_str(iban: str, bank_code: str) -> list:
-    """
-    Retrieves a string with the bank information
+def validate(iban_string: str, spec: 'IbanSpec'):
+    bban: str = iban_string[4:]
+    iban_match: 'Match' = re.compile(spec.iban_pattern).match(iban_string)
+    bban_match: 'Match' = re.compile(spec.bban_pattern).match(bban)
 
-    :param iban: An iban string
-    :type iban: str
-    :param bank_code: the bank code
-    :type bank_code: str
-    :return: A list with the bank information
-    :rtype: str
-    """
-    filename = f'{iban[:6]}.txt'
-    location = f'data/banks/{filename}'
-    try:
-        stream = pkg_resources.resource_stream(__name__, location)
-    except FileNotFoundError:
-        return []
-    for line in stream:
-        line = line.decode('utf-8')
-        if line.startswith(bank_code + '|'):
-            return line.strip().split("|")
-    return []
+    if spec.length != len(iban_string):
+        raise ValueException('Invalid Iban length')
 
+    if bban_match is None:
+        raise InvalidBBANException('Invalid Basic Bank Account Number')
 
-def bban_to_regex(bban_format: str) -> str:
-    """
-    converts the bban description into a regex
+    if iban_match[spec.check_digit] is None:
+        raise InvalidCheckDigitException('Invalid Check Digit')
 
-    :param bban_format: the bban format
-    :type bban_format: str
-    :return: a string representing a regex
-    :rtype: str
-    """
-    bban_formats_list = bban_format.split(",")
-    regex_list = []
-    for x in bban_formats_list:
-        type = x[-1]
-        size = x[:-1]
-        if type == 'a':
-            regex_list.append(f"[A-Z]{{{size}}}")
-        elif type == 'c':
-            regex_list.append(f"[A-Za-z0-9]{{{size}}}")
-        elif type == 'n':
-            regex_list.append(f"[0-9]{{{size}}}")
-    bban_regex = ''.join(regex_list)
-    return bban_regex
+    if spec.reserved_fields is not None:
+        reserved_fields_match: 'Match' = re.compile(spec.reserved_fields.pattern).match(iban_string)
+        if reserved_fields_match is None:
+            raise InvalidReservedValues("Expected reserved values where not found")
+
+    if __calculate_mod97(iban_string) != 1:
+        raise InvalidMod97Exception('Invalid MOD 97 Operation Result')
 
 
-def validate_mod_97(iban: str) -> int:
-    """
-    Validats an IBAN using MOD 97 operation
-
-    :param iban: The iban to be validated
-    :type iban: str
-    :return: The remainder of the mod operation
-    :rtype: int
-    """
+def __calculate_mod97(iban: str) -> int:
     iban = iban[4:] + iban[:4]
-    iban = iban.upper()
-    for letter, number in letter_to_replace.items():
+    assert iban.isupper(), f"{iban} must be upper case"
+    for letter, number in LETTERS.items():
         iban = iban.replace(letter, number)
     remainder = int(iban) % 97
     return remainder
 
 
-def select_from_format(regex, iban_format: str, iban: str) -> str:
-    """
-    Selects part of an IBAN string using regex
+def __get_match_group(match: 'Match', code) -> Optional[str]:
+    if code == 0:
+        return None
+    return match[code]
 
-    Use to extract info from IBAN such as country code, bank code and others
 
-    :param regex: the regex
-    :type regex: str/r
-    :param iban_format: the iban format string used to locate the info position
-    :type iban_format: str
-    :param iban: The iban
-    :type iban: str
-    :return: a string representing the part of the iban
-    :rtype: str
+def __get_iban_spec(country_code: str) -> Optional['IbanSpec']:
     """
-    match = re.search(regex, iban_format)
-    if match:
-        start = match.start()
-        end = match.end()
-        return iban[start:end]
+    Retrieves Banks data from resources
+
+    Providing the country code
+
+    :param country_code: The iban country code
+    :type country_code: str
+    :return: Specification for IBAN from the country or none if not found
+    :rtype: list
+    """
+    specs: 'IbansSpecs' = IbansSpecs()
+    iban_spec: 'IbanSpec'
+    try:
+        stream = pkg_resources.resource_string(__name__, f'data/ibans/{country_code[0]}.dat')
+        specs.ParseFromString(stream)
+    except FileNotFoundError:
+        specs = None
+
+    if specs and country_code in specs.specs:
+        iban_spec = specs.specs[country_code]
     else:
-        return ""
+        iban_spec = None
+
+    return iban_spec
+
+
+def __get_bank(country_code: str, bank_code: str, branch: str) -> Optional['Bank']:
+    """
+    get_iban_validation_str retrieves a string from ibans.txt
+
+    The string containing the IBAN definition for the selected country
+
+    :param country_code: The resource folder
+    :type country_code: str
+    :param bank_code: The resource file
+    :type bank_code: str
+    :return: a list containing the IBAN validation data
+    :rtype: list
+    """
+    banks: 'BanksData' = BanksData()
+    bank: 'Bank'
+    data: 'BankData'
+    bank_key: str = country_code + bank_code
+    try:
+        stream = pkg_resources.resource_string(__name__, f'data/banks/{country_code}.dat')
+        banks.ParseFromString(stream)
+    except FileNotFoundError:
+        banks = None
+
+    if banks and bank_key in banks.banks:
+        data = banks.banks[bank_key]
+        bank = Bank(data.code, name=data.name, short_name=data.short_name, branch=branch, swift=data.swift)
+    else:
+        bank = Bank(bank_code, branch=branch)
+    return bank
